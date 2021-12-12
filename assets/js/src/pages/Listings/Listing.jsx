bcb0e4be7d05b6dd23966d3909db008e4a2c0e13
Graham Preston <graham.preston@gmail.com>
2021-12-11 23:54:12 -0500

    Listings index and show page

--- assets/js/src/pages/Listings/Listing.jsx
+++ assets/js/src/pages/Listings/Listing.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import { selectItemIds, selectItem } from '../../features/listings/listingsSlice';
+import { fetchItem } from '../../features/listings/listingsService';
+
 export default function Listing() {
-  const { id } = useParams();
+  const params = useParams();
+  const id = parseInt(params.id);
 
-  return (
-    <>
-      <h3>Some cool product with id {id}</h3>
-      <p>description...</p>
-      <p>Buy it now: $1,000,000</p>
-    </>
-  )
+  const dispatch = useDispatch()
+  const itemIds = useSelector(selectItemIds);
+  const item = useSelector(state => selectItem(state, id));
+
+  useEffect(() => {
+    if (!itemIds.includes(id)) {
+      fetchItem(dispatch, id);
+    }
+  }, [itemIds]);
+
+  if (item) {
+    return (
+      <>
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+        <p>Seller ID: {item.seller_id}</p>
+        <p>Buy it now: {item.price} cents</p>
+
+        <img
+          src={item.image_url}
+          alt={`item-${item.id}`}
+        />
+      </>
+    );
+  } else {
+    return null;
+  }
 }
-
--- assets/js/src/pages/Listings/index.jsx
+++ assets/js/src/pages/Listings/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, Outlet } from 'react-router-dom';
+
+import { selectAllItems } from '../../features/listings/listingsSlice';
+import { fetchAllItems } from '../../features/listings/listingsService';
 
 export default function Listings() {
+  const dispatch = useDispatch();
+  const items = useSelector(selectAllItems);
+
+  useEffect(() => {
+    fetchAllItems(dispatch);
+  }, []);
+
   return (
     <ul>
-      <li>Thing 1</li>
-      <li>Thing 2</li>
-      <li>Thing 3</li>
+      {items.map(item => (
+      <li key={item.id}>
+        <Link to={`/listings/${item.id}`}>
+          {item.name}: {item.price} cents
+        </Link>
+      </li>
+      ))}
+      <Outlet />
     </ul>
   )
 }
