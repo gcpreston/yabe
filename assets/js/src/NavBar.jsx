989a9e16f00a7817beb379f45b955d9f0ea7699d
Graham Preston <graham.preston@gmail.com>
2021-12-13 18:31:44 -0500

    NavBar uses location

--- assets/js/src/NavBar.jsx
+++ assets/js/src/NavBar.jsx
@@ -1,26 +1,28 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <ul className='nav nav-pills nav-justified'>
       <li className='nav-item'>
-        <a className='nav-link active' href='/'>Home</a>
+        <a className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} href='/'>Home</a>
       </li>
       <li className='nav-item'>
-        <a className='nav-link' href='/listings'>Listings</a>
+        <a className={`nav-link ${location.pathname === '/listings' ? 'active' : ''}`} href='/listings'>Listings</a>
       </li>
       <li className='nav-item'>
-        <a className='nav-link' href='/sellers'>Sellers</a>
+        <a className={`nav-link ${location.pathname === '/sellers' ? 'active' : ''}`} href='/sellers'>Sellers</a>
       </li>
       <li className='nav-item'>
-        <a className='nav-link' href='/buyers'>Buyers</a>
+        <a className={`nav-link ${location.pathname === '/buyers' ? 'active' : ''}`} href='/buyers'>Buyers</a>
       </li>
       <li className='nav-item'>
-        <a className='nav-link' href='/profile'>Profile</a>
+        <a className={`nav-link ${location.pathname === '/profile' ? 'active' : ''}`} href='/profile'>Profile</a>
       </li>
     </ul>
   )
 }
 
 export default NavBar;
-
