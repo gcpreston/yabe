4123e3ebdc3ad480bb17cb29fb38a21a89f604e4
Eddie Zhou <ace.dragonfly@gmail.com>
2021-12-14 11:39:53 -0500

    add search button

--- assets/js/src/SearchBar.jsx
+++ assets/js/src/SearchBar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const SearchBar = () => {
   return (
     <div className='col-12 col-sm-8 col-md-9 col-lg-10'>
-      <input type='text' className='form-control m-2 d-inline-flex' placeholder='Search Items'/>
+      <div className='input-group my-2'>
+        <input id='search' type='text' className='form-control input-group' placeholder='Search Items'/>
+        <div className='input-group-append'>
+          <button className='rounded-0 rounded-end btn btn-outline-primary'>Search</button>
+        </div>
+      </div>
     </div>
   )
 }
