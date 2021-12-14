a493415a6817a318fc19b6b441d1be5772450305
Graham Preston <graham.preston@gmail.com>
2021-12-13 19:38:14 -0500

    Buy button for buyers only

--- assets/js/src/AuthLoader.jsx
+++ assets/js/src/AuthLoader.jsx
@@ -29,9 +29,14 @@ const AuthLoader = () => {
   useEffect(() => {
     const currentUserId = document.getElementById('current-user-id');
     const currentUserEmail = document.getElementById('current-user-email');
+    const currentUserRole = document.getElementById('current-user-role');
 
     if (currentUserId && currentUserEmail) {
-      const currentUser = { id: parseInt(currentUserId.textContent), email: currentUserEmail.textContent };
+      const currentUser = {
+        id: parseInt(currentUserId.textContent),
+        email: currentUserEmail.textContent,
+        role: currentUserRole.textContent
+      };
       dispatch(setUser(currentUser));
     }
   }, []);
--- assets/js/src/pages/Listings/Listing.jsx
+++ assets/js/src/pages/Listings/Listing.jsx
@@ -4,12 +4,16 @@ import { useParams } from 'react-router-dom';
 
 import { selectItemIds, selectItem } from '../../features/listings/listingsSlice';
 import { fetchItem } from '../../features/listings/listingsService';
+import { selectIsAuthenticated, selectUser } from '../../features/auth/authSlice';
 
 export default function Listing() {
   const params = useParams();
   const id = parseInt(params.id);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
   const itemIds = useSelector(selectItemIds);
   const item = useSelector(state => selectItem(state, id));
 
@@ -25,7 +29,7 @@ export default function Listing() {
         <h3>{item.name}</h3>
         <p>{item.description}</p>
         <p>Seller: {item.seller.email}</p>
-        <p>Buy it now: {item.price} cents</p>
+        {isAuthenticated && user.role === 'buyer' ? <button className='btn btn-primary'>Buy</button> : null}
 
         <img
           src={item.image_url}
