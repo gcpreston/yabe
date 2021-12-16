fdd9f69e80094b080c9b5b797b015cddda55f11c
Graham Preston <graham.preston@gmail.com>
2021-12-16 13:22:24 -0500

    Search on enter

--- assets/js/src/SearchBar.jsx
+++ assets/js/src/SearchBar.jsx
@@ -6,16 +6,22 @@ const SearchBar = () => {
   const navigate = useNavigate();
   const handleChange = (e) => {
     setInput(e.target.value);
-  }
+  };
   const handleClick = () => {
     const path = '/search/' + input;
     navigate(path);
-  }
+  };
+  const handleOnKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      const path = '/search/' + input;
+      navigate(path);
+    }
+  };
 
   return (
     <div className='col-12 col-sm-8 col-md-9 col-lg-10'>
       <div className='input-group my-2'>
-        <input id='search' type='text' className='form-control input-group' placeholder='Search Items' onChange={handleChange}/>
+        <input id='search' type='text' className='form-control input-group' placeholder='Search Items' onChange={handleChange} onKeyDown={handleOnKeyDown} />
         <div className='input-group-append'>
           <button className='rounded-0 rounded-end btn btn-outline-primary' onClick={handleClick}>Search</button>
         </div>
